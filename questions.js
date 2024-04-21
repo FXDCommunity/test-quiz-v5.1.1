41287ea018af95c74b35d65026c2e1bb1a80310d
FXDCommunity <166769846+FXDCommunity@users.noreply.github.com>
2024-04-21 13:22:53 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 36,
+        question: "Script Fondasi Web Menggunakan?",
+        answer: "C. !DOCTYPE html",
+        options: [
+            "A. const web = document.querySelector",
+            "B. function",
+            "C. !DOCTYPE html",
+            "D. margin"
+        ]
+    },
+    {
+        numb: 37,
+        question: "Fondasi Button Pertama Di Java Adalah?",
+        answer: "A. const textBtn = document.querySelector('.text-text');",
+        options: [
+            "A. const textBtn = document.querySelector('.text-text');",
+            "B. function showText(index) {}",
+            "C. querySelector",
+            "D. let textTag"
+        ]
+    },
+    {
+        numb: 38,
+        question: "Cara Button Bekerja?",
+        answer: "A. startBtn.onclick = () => {('active');('active');",
+        options: [
+            "A. startBtn.onclick = () => {('active');('active');",
+            "B. startBtn.onclick = ()",
+            "C. const startBtn = document.querySelector('.start-btn');",
+            "D. startBtn.onclick = querySelector.start.btn (index);"
+        ]
+    },
+    {
+        numb: 4,
+        question: "<section class=text-section> Di Gunakan Untuk?",
+        answer: "B. Agar Jawaban Quiz Bisa Bekerja",
+        options: [
+            "A. ",
+            "B. Agar Jawaban Quiz Bisa Bekerja",
+            "C. ",
+            "D. "
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+];
